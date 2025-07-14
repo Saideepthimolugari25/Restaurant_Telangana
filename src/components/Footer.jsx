0cbfd0d3a643085f270686dbb6c6120525d9b720
Saideepthimolugari25 <molugarisaideepthi@gmail.com>
2025-07-14 22:40:41 +0530

    Initial commit - Frontend

---
+++ src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">SAI DEEPTHI</div>
+          <div className="right">
+            <p>Telangana, Hyderabad</p>
+            <p>Open: 05:00 PM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By Sai Deepthi Molugari</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By SAI DEEPTHI MOLUGARI</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
